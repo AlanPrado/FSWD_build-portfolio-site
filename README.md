dc08dfe007d8a25afad46da2f04519f51f3a1e3d
Alan <megalanxx@gmail.com>
2016-12-28 20:49:44 -0200

    initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+#Project: Build a magic portifolio
+
+Build a magic portifolio demo site.
+
+## Quick start
+
+- Clone the repo: `https://github.com/AlanPrado/build-portfolio-site`
+- Install node.js
+- Run: `npm run start` for debug
+- Run: `npm run release` to build release
+
+Note that you should not change any .css files in order to change the style.
+Change s*.scss files and *.css files will be automatic generated with the modifications.
+
+### What's included
+
+Within the download you'll find the following directories and files.
+```
+```
+
+A brief description about each file:
+
+
+## Copyright and license
+Code and documentation copyright 2016-2016 Code released under the [MIT License](https://github.com/AlanPrado/build-portfolio-site/master/LICENSE)
+
+## Authors
+
+#### Original Author and Development Lead
+
+- Alan Thiago do Prado (aprado.cnsp@gmail.com)
+
+## External Resources
+
+The following resources were used to build this page.
+
+1. https://css-tricks.com/using-svg/
+2. http://picsvg.com/
+3. http://gallery.yopriceville.com/Abstract/Green_themes#.WGL0LRsrLIU
+4. http://gallery.yopriceville.com/Abstract/Frosty_snow_storm_widescreen_wallpaper#.WGL1fBsrLIU
+5. http://gallery.yopriceville.com/Abstract/Gold_Curves#.WGL11hsrLIV
+6. http://gallery.yopriceville.com/Abstract/Flames_Wallpaper#.WGL2CxsrLIU
+7. http://thesassway.com/intermediate/if-for-each-while
+8. http://www.favicon-generator.org/
---
+++ gulpfile.js
@@ -0,0 +1,122 @@
+'use strict';
+
+const gulp = require('gulp');
+const watch = require('gulp-watch');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+const pump = require('pump');
+const cssnano = require('gulp-cssnano');
+const sourcemaps = require('gulp-sourcemaps');
+const imagemin = require('gulp-imagemin');
+const pngquant = require('imagemin-pngquant');
+const imageResize = require('gulp-image-resize');
+const rename = require("gulp-rename");
+const rm = require("gulp-rm");
+
+const destDir = './docs/';
+const appDir = './app/';
+
+function styles(cb, dist) {
+	let tasks = [];
+
+	tasks.push(gulp.src(appDir + 'sass/**/*.scss'));
+	tasks.push(sourcemaps.init());
+	tasks.push(sass());
+	tasks.push(
+		autoprefixer({
+			browsers: ['last 2 versions']
+		}));
+
+	if(dist) tasks.push(cssnano());
+
+ 	tasks.push(sourcemaps.write());
+  tasks.push(gulp.dest(destDir + 'css'));
+	tasks.push(browserSync.stream());
+
+	pump(tasks, cb);
+}
+gulp.task('styles', (cb) => styles(cb));
+gulp.task('styles-dist', (cb) => styles(cb, true));
+
+function copy(options, cb) {
+	options = options || {};
+	options = {
+		src: options.src || '',
+		dest: options.dest || '',
+		enableSourceMaps: options.enableSourceMaps || false,
+		imagemin: options.imagemin || false,
+		hasBasePath: options.basePath || false
+	};
+
+	let taskList = [];
+	if(typeof options.src === "object") {
+		options.src.forEach(function(el, idx) {
+			options.src[idx] = appDir + el;
+		});
+		taskList.push(gulp.src(options.src));
+	} else {
+		taskList.push(gulp.src([appDir + options.src, '!' + appDir + '/tests/**/*']));
+	}
+	if(options.enableSourceMaps) taskList.push(sourcemaps.init());
+	if(options.enableSourceMaps) taskList.push(sourcemaps.write());
+
+  if(options.imagemin)
+		taskList.push(imagemin({
+					            progressive: true,
+					            use: [pngquant()]
+					        }));
+
+	let destinationPath = options.hasBasePath ? '' : destDir;
+	taskList.push(gulp.dest(destinationPath + (options.dest)));
+	pump(taskList, cb);
+};
+
+function clearDir(path, cb) {
+	pump([gulp.src(path, { read: false }), rm()], cb);
+};
+gulp.task('clear-images', (cb) => clearDir(appDir + '/img/*', cb));
+gulp.task('clear-dist', (cb) => clearDir(destDir + '/**/*', cb));
+
+gulp.task('copy-html', (cb) => copy({src: '**/*.html'}, cb));
+
+gulp.task('resize-images', ['clear-images'], (cb) => {
+	gulp.start(['copy-fixed-images']);
+	let taskList = [];
+	function resize(img, width) {
+		var options = {
+			 upscale : false,
+			 format: 'png',
+			 imageMagick: true
+		};
+		options.width = width;
+		taskList.push(gulp.src(appDir + img));
+		taskList.push(imageResize(options));
+		taskList.push(rename(function (path) { path.basename += "-" + width + "w"; }));
+		taskList.push(gulp.dest(appDir + '/img/'));
+	}
+	resize('/img_src/green.jpg', 900);
+	resize('/img_src/spell*.jpg', 400);
+	pump(taskList, cb);
+});
+
+gulp.task('copy-images', (cb) => copy({src: 'img/**/*', dest: 'img'}, cb));
+gulp.task('copy-fixed-images', (cb) => copy({src: 'img_src/fixed/*', dest: appDir + 'img', basePath: true}, cb));
+gulp.task('copy-images-dist', (cb) => copy({src: 'img/**/*', dest: 'img', imagemin: true}, cb));
+gulp.task('copy-favicon', (cb) => copy({ src: ['*.png', '*.xml','*.ico','*.json'] }, cb));
+
+//use this task for debug
+gulp.task('default', ['copy-html', 'copy-images', 'copy-favicon', 'styles'], () => {
+	gulp.watch(appDir + 'sass/**/*.scss', ['styles']);
+	gulp.watch(appDir + '**/*.html', ['copy-html']);
+	gulp.watch(destDir + '**/*.html')
+		  .on('change', browserSync.reload);
+
+	browserSync.init({
+			 server: destDir
+		});
+});
+//use this task for build a realease
+gulp.task('dist', ['resize-images', 'clear-dist'], () => {
+	gulp.start(['copy-html', 'copy-images-dist', 'copy-html', 'copy-favicon', 'styles-dist']);
+});
